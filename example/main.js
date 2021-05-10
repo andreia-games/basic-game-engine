5e0e09def4eeb0797fecd9619bb58884ff79902f
Sonya <sonya.castro.gomez@gmail.com>
2021-05-10 00:00:43 -0500

    MAIN

---
+++ example/main.js
@@ -0,0 +1,30 @@
+let map;
+let player;
+let componetsJSON;
+let boxSize = 100;
+function preload() {
+    componetsJSON = loadJSON("assets/prueba.json");
+}
+function setup() {
+    createCanvas(30 * boxSize, 20 * boxSize);
+    background(color('blue'));
+    map = new GameMap("Mapa", 29, 19, boxSize, componetsJSON);
+    player = new Player("Pepito");
+    map.setPlayer(player, 3, 6);
+    player.addControler(player);
+
+}
+
+function draw() {
+    map.drawGrid();
+    fill(color('red'));
+    rect(player.x * boxSize, player.y * boxSize, boxSize, boxSize);
+}
+
+function keyPressed() {
+    player.controler.keyPressed();
+}
+
+function windowResized() {
+    map.windowResized();
+}
